bf1d04492b02a8beaf87d791842d1fbb3d27ebe7
Lili <38546901+Liligo@users.noreply.github.com>
2025-07-02 15:42:38 +0800

    修正图片的二级缓存方案：Caffeine + Redis

--- src/main/java/com/liligo/reggie/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/liligo/reggie/service/impl/OrderDetailServiceImpl.java
@@ -1,10 +1,11 @@
-package com.liligo.reggie.service;
+package com.liligo.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.liligo.reggie.entity.OrderDetail;
 import com.liligo.reggie.mapper.OrderDetailMapper;
+import com.liligo.reggie.service.OrderDetailService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderDetailServiceImpl extends ServiceImpl<OrderDetailMapper, OrderDetail> implements OrderDetailService{
+public class OrderDetailServiceImpl extends ServiceImpl<OrderDetailMapper, OrderDetail> implements OrderDetailService {
 }
